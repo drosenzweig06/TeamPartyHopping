94c5db498c049d16b3d72df037bb11695f9ed515
tokoltermann <tokoltermann@gmail.com>
2025-03-04 12:21:57 -0600

    gerrr

--- src/Clickable.java
+++ src/Clickable.java
@@ -6,37 +6,19 @@ public interface Clickable {
     /**
      * Renders the clickable object to the application window
      */
-    private void draw() {
-
-    }
+    void draw();
     /**
      * Determines whether the cursor is currently over this object
      *
      * @return true if the cursor is over the object, false otherwise
      */
-    private boolean isMouseOver() {
-        float agentX = agent.getX();
-        float agentY = agent.getY();
-        float agentWidth = agent.width();
-        float agentHeight = agent.height();
-        float mouseX = PApplet.
-        float mouseY = processor.mouseY();
-        if (mouseX > agentX - .5 * agentWidth && mouseX < agentX + .5 * agentWidth){
-            if (mouseY > agentY - .5 * agentHeight && mouseY < agentY + .5 * agentHeight){
-                return true;
-            }
-        }
-        return false;
-    }
+    boolean isMouseOver();
     /**
      * Implements the behavior to be run each time the mouse is released
      */
-    private void mouseReleased() {
-    }
+    void mouseReleased();
     /**
      * Implements the behavior to be run each time the mouse is pressed
      */
-    private void mousePressed() {
-
-    }
+    void mousePressed();
 }
